075b43b17649bf8e3328830cc489faf705835094
SouravJ7860 <souravjangra0070@gmail.com>
2026-05-05 10:42:46 +0530

    First Commit

--- config/config.js
+++ config/config.js
@@ -3,27 +3,28 @@ import express from "express";
 
 async function staticConfig(app) {
   app.set("view engine", "ejs");
-  app.set("views", path.join(process.cwd(), "src", "views"));
+  // app.set("views", path.join(process.cwd(), "src", "views"));
+  app.set("views", path.join(process.cwd(), "views"));
 
   // Serve: project-root/public
   app.use(
     express.static(path.join(process.cwd(), "public"), {
-      maxAge: "1d"
-    })
+      maxAge: "1d",
+    }),
   );
 
   // Serve: project-root/src/public
   app.use(
     express.static(path.join(process.cwd(), "src", "public"), {
-      maxAge: "1d"
-    })
+      maxAge: "1d",
+    }),
   );
   // Explicit fix: serve /assets separately (bypasses auth middleware)
   app.use(
     "/assets",
     express.static(path.join(process.cwd(), "src", "public", "assets"), {
-      maxAge: "1d"
-    })
+      maxAge: "1d",
+    }),
   );
 }
 
