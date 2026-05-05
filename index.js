e85d0109c8730fe19a481b630d2fc5a6e2a5ea47
SouravJ7860 <souravjangra0070@gmail.com>
2026-05-05 10:26:04 +0530

    First Commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ cronController.startDailyEarningsCron();
 
 const port = process.env.PORT;
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
 
 
